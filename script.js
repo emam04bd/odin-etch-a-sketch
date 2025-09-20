ddb69c81f3d236cdbd851fdadfca0b13c2920c71
emam04bd <emam04bd@gmail.com>
2025-09-20 11:27:44 +0600

    refactor: designated function to createGrid

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const gridContainerEl = document.querySelector("#grid-container");
 
-for(let i = 0; i < 16; i++) {
-    const rowContainerEl = document.createElement("div");
-    rowContainerEl.classList.add("row-container");
-    gridContainerEl.appendChild(rowContainerEl);
-    for(let j = 0; j < 16; j++) {
-        const cellEl = document.createElement("div");
-        cellEl.classList.add("cell");
-        rowContainerEl.appendChild(cellEl);
-        cellEl.addEventListener("mouseover", () => {
-            cellEl.style.backgroundColor = "#000";
-        })
-    }
+function createGrid(n) {
+	for (let i = 0; i < n; i++) {
+		const rowContainerEl = document.createElement("div");
+		rowContainerEl.classList.add("row-container");
+		gridContainerEl.appendChild(rowContainerEl);
+		for (let j = 0; j < n; j++) {
+			const cellEl = document.createElement("div");
+			cellEl.classList.add("cell");
+			rowContainerEl.appendChild(cellEl);
+			cellEl.addEventListener("mouseover", () => {
+				cellEl.style.backgroundColor = "#000";
+			});
+		}
+	}
 }
+
+createGrid(16);
